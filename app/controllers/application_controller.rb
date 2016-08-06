d3aa453df59f9ce0483b798b72c144ddd555853f
Josh Smith <jrs023@uark.edu>
2016-08-06 17:01:00 -0400

    Web server set up

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,34 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def hello 
+    #Call Get Products
+    
+    #SKUS
+    skus = "[978-1601424280,978-0802848000]"
+    
+    gp_response = HTTParty.post("https://app.skuvault.com/api/products/getProducts", :query =>{:ProductSKUs => skus,:UserToken => "rU+wISuzqhzmJI2y5A0I3QX6bdmBB4lXpqeQtDk1ukM=", :TenantToken => "lxe4jjI1279ozpNWB9gecweAFl12kU+gngKjmL1ZNCg=", :format => "json"})
+   
+    #render json: gp_response
+    info = []
+    
+    title = []
+    qonhand= []
+    qpending = []
+    gp_response['Products'].each do |p|
+    	title.append(p['Description'])
+    end
+    gp_response['Products'].each do |p|
+    	qonhand.append(p['QuantityOnHand'])
+    end
+    gp_response['Products'].each do |p|
+    	qpending.append(p['QuantityPending'])
+    end
+    info.append(title)
+    info.append(skus)
+    info.append(qonhand)
+    info.append(qpending)
+    
+    render json: info
+  end
+end
