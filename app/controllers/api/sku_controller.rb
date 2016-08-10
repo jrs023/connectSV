fa3303ded0a789338baf854d0dcafd2986f4dc52
Josh Smith <jrs023@uark.edu>
2016-08-09 20:48:19 -0400

    Add the Quantity Available Column

--- app/controllers/api/sku_controller.rb
+++ app/controllers/api/sku_controller.rb
@@ -2,7 +2,7 @@ class Api::SkuController < ApplicationController
   require 'json'
   def index   
     #SKUS
-    #skus = "978-0802412706,978-0842360241,978-0986366703,978-0986366710,978-1400324347,978-0990351405,978-1414398044,978-1433548406,978-1433679599,978-1481866880,978-1490907550,978-1490925387,978-1589975385,978-1594153204,978-1594631870"
+    #skus = "978-0802412706,978-1594153204,978-1594631870"
     skus = params[:skus]
 
     gp_response = HTTParty.post("https://app.skuvault.com/api/products/getProducts", :query =>{:ProductSKUs => skus,:UserToken => "rU+wISuzqhzmJI2y5A0I3QX6bdmBB4lXpqeQtDk1ukM=", :TenantToken => "lxe4jjI1279ozpNWB9gecweAFl12kU+gngKjmL1ZNCg=", :format => "json"})
@@ -10,17 +10,19 @@ class Api::SkuController < ApplicationController
     title = []
     onhand= []
     pending = []
+    available = []
     previous_p = "";
     gp_response['Products'].each do |p|
     	if(previous_p != p['Description'])
     		title.append(p['Description'])  
     		onhand.append(p['QuantityOnHand'])    		
-    		pending.append(p['QuantityPending'])
+    		pending.append(p['QuantityPending'])		
+    		available.append(p['QuantityAvailable'])
     	end
     	previous_p = p['Description']
     end
     
-    my_object = { :title => title, :skus => skus.split(","), :onhand => onhand, :pending => pending}
+    my_object = { :title => title, :skus => skus.split(","), :onhand => onhand, :pending => pending, :available => available}
 	render json: my_object.to_json, :callback => params[:callback]
   end
 end
